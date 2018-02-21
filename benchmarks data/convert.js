97082309301f7d5abedd52a6b1db4147280433eb
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2018-02-21 11:01:45 +0000

    Added alpha variants to node benchmark results

--- benchmarks data/convert.js
+++ benchmarks data/convert.js
@@ -4,12 +4,18 @@ const args = process.argv.slice(2, 12)
 const {PNGArrays} = require("../dev/index.js")
 
 let dataPath
+let useAlpha = false
 
 for (let a=0; a<args.length; a++) {
     switch (args[a]) {
 
         case "-in":
             dataPath = args[a+1]
+            a++
+            break
+
+        case "-a":
+            useAlpha = args[a+1].trim().toLowerCase()==="y"
             a++
             break
     }
@@ -26,6 +32,9 @@ const {rawData} = require(`./${dataPath}/rawData.js`)
 
 console.log("Converting...")
 
-PNGArrays.toPNG(rawData, {file: `./${dataPath}/node.png`})
+PNGArrays.toPNG(rawData, {
+    alpha: useAlpha,
+    file: `./${dataPath}/node${useAlpha ? "-alpha" : ""}.png`
+})
 .then(() => console.log("done."))
 .catch(e => console.log("nope", e))
