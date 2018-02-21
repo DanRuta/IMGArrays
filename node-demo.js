8457ddfb80523e04b20e73b825e696649d691bdd
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2018-02-21 11:23:18 +0000

    Updated node-demo

--- node-demo.js
+++ node-demo.js
@@ -1,18 +1,20 @@
 "use strict"
 
 const {PNGArrays} = require("./dev/index.js")
-const {testDataRaw} = require("./testDataRaw.js")
-const {testDataHalfway} = require("./testDataHalfway.js")
+const {rawData} = require("./benchmarks data/medium/rawData.js")
 
-// console.log("Converting data to PNG...")
+console.log("Converting data to PNG...")
 
-// PNGArrays.toPNG(testDataRaw, {file: "./node-demo.png"})
-// .then(() => console.log("done."))
-// .catch(e => console.log("nope", e))
+PNGArrays.toPNG(rawData, {file: "./node-demo.png"})
+.then(() => {
+    console.log("done.")
 
-console.log("reading data from PNG...")
+    console.log("reading data from PNG...")
 
-PNGArrays.fromPNG("./node-demo.png")
-.then(console.log)
+    PNGArrays.fromPNG("./node-demo.png")
+    .then(console.log)
+    .catch(e => console.log("nope", e))
+
+})
 .catch(e => console.log("nope", e))
 
